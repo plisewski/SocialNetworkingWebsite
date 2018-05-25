01d23e4a05a740041ea07845b345a77a328b3129
Piotr Lisewski <p.lisewski@hotmail.com>
2018-05-25 14:20:58 +0200

    Create Add a Tune form.

---
+++ SocialNetworkingWebsite/Controllers/TunesController.cs
@@ -0,0 +1,27 @@
+﻿using SocialNetworkingWebsite.Models;
+using SocialNetworkingWebsite.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace SocialNetworkingWebsite.Controllers
+{
+    public class TunesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TunesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new TuneFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ SocialNetworkingWebsite/ViewModels/TuneFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using SocialNetworkingWebsite.Models;
+using System.Collections.Generic;
+
+namespace SocialNetworkingWebsite.ViewModels
+{
+    public class TuneFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
